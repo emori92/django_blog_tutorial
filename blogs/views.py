cb43bb5a947d5cc7d6b922ae0d605527d5c37ba0
falcon92 <tktk.tktk.ixjs@gmail.com>
2020-05-29 23:56:35 +0900

    index.htmlを表示できるよう調整

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from django.views import View
+from django.views.generic import TemplateView, RedirectView
+
 
 # Create your views here.
+class Index(TemplateView):
+    
+    template_name = 'index.html'
+
+index = Index.as_view()
